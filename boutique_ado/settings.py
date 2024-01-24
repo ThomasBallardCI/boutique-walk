91217a55b9c85309eb08ec6f4149e1fc3287d892
ThomasBallardCI <ThomasGEBallard@gmail.com>
2024-01-24 13:48:41 +0000

    set heroku secret key and debug true if in development enviroment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14(v_0$0@z(+_0ce23g(#zjrjj((5ggq7)*i-i#@tm(b^c^a+h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutiqueado-ci-tb-6976431eff78.herokuapp.com', 'localhost']
 
