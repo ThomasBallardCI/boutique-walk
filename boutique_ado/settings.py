2de4f87b4cd217ed93d498dcd543ba7ec86dffdb
ThomasBallardCI <ThomasGEBallard@gmail.com>
2024-01-24 13:19:21 +0000

    heroku deployment settings added

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.isfile('env.py'):
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-14(v_0$0@z(+_0ce23g(#zjrjj((5ggq7)*i-i#@tm(b^c^a+h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-thomasballa-boutiquewal-doi0hkcchhk.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['boutiqueado-ci-tb.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +122,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -184,3 +190,5 @@ DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
